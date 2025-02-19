32a6c19b42d45f734193b37da420529a448a30d2
kantonirina02 <kantonirina@outlook.fr>
2025-02-20 00:54:36 +0100

    connexion/deconnexion Afficher /masquer en fonction des role

--- Router/Router.js
+++ Router/Router.js
@@ -45,6 +45,8 @@ const LoadContentPage = async () => {
   // Changement du titre de la page
   document.title = actualRoute.title + " - " + websiteName;
 };
+// Afficher et masquer les elements en fonction des rôles
+showAndHideElementsForRoles();
 
 // Fonction pour gérer les événements de routage (clic sur les liens)
 const routeEvent = (event) => {
--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,7 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/covoiturages","Covoiturages","/pages/covoiturages.html"),
-    new Route("/signin","Connexion","/pages/signin.html"),
+    new Route("/signin","Connexion","/pages/signin.html", "/js/auth/signin.js"),
     new Route("/signup","Inscription","/pages/signup.html", "/js/auth/signup.js"),
     new Route("/account","Mon Profil","/pages/account.html"),
     new Route("/editPassword","Modifier mon mot de passe","/pages/editPassword.html"),
