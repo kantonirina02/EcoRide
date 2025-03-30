0d7f97258cbee1028d7fe114f0d0def6f211523d
kantonirina02 <kantonirina@outlook.fr>
2025-03-30 23:04:21 +0200

    homecontr

--- BackEnd/ecoride/src/Controller/HomeController.php
+++ BackEnd/ecoride/src/Controller/HomeController.php
@@ -11,6 +11,6 @@ class HomeController extends AbstractController
     #[Route('/')]
     public function home() : Response
     {
-    return new Response('Bienvenue sur votre accueil!') ;
-     }
+        return new Response('Bienvenue sur votre accueil!');
+    }
 }
